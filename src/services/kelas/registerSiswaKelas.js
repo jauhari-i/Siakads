a8072f1bf1566c885fd1615eb55fa0b3b79de049
telkomdev-irfan_jauhari <kangrykong123@gmail.com>
2020-07-09 12:58:46 +0700

    fix registerkelas

--- src/services/kelas/registerSiswaKelas.js
+++ src/services/kelas/registerSiswaKelas.js
@@ -3,16 +3,18 @@ const Kelas = require('../../models/Kelas');
 const { errorCb, successCb } = require('../../config/callback');
 
 module.exports = registerSiswaKelas = async (data, cb) => {
-  const kelasD = await Kelas.findById(data.idKelas);
-  !kelasD && errorCb({ status: 404, success: false, msg: 'Id Kelas Tidak Valid' }, cb);
-  const siswaD = await Siswa.findById(data.idSiswa);
-  !siswaD && errorCb({ status: 404, success: false, msg: 'Id Siswa Tidak Valid' }, cb);
+  try {
+    const kelasD = await Kelas.findById(data.idKelas);
+    const siswaD = await Siswa.findById(data.idSiswa);
 
-  await Siswa.update({ _id: siswaD._id }, { kelas: kelasD.namaKelas })
-    .then((siswa) => {
-      successCb({ status: 200, success: true, msg: 'Registrasi Kelas Berhasil' }, cb);
-    })
-    .catch((err) => {
-      errorCb({ status: 500, success: false, err, msg: 'Internal server error' }, cb);
-    });
+    await Siswa.update({ _id: siswaD._id }, { kelas: kelasD.namaKelas })
+      .then((siswa) => {
+        successCb({ status: 200, success: true, msg: 'Registrasi Kelas Berhasil' }, cb);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  } catch (error) {
+    errorCb({ status: 500, success: false, err, msg: 'Internal server error' }, cb);
+  }
 };
