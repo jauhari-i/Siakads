6699e5b50a7de49406e71e708d362a1cc5b234b1
telkomdev-irfan_jauhari <kangrykong123@gmail.com>
2020-07-09 12:57:32 +0700

    testing try

--- src/services/admin/pelanggaranIndex.js
+++ src/services/admin/pelanggaranIndex.js
@@ -75,34 +75,33 @@ module.exports = pelanggaranInstance = {
       });
   },
   addPelanggaran: async (data, cb) => {
-    const siswaD = await Siswa.findById(data.idSiswa);
-    const jenisP = await JenisPelanggaran.findById(data.idJenisP);
-    const guruD = await Guru.findById(data.idGuru);
+    try {
+      const siswaD = await Siswa.findById(data.idSiswa);
+      const jenisP = await JenisPelanggaran.findById(data.idJenisP);
+      const guruD = await Guru.findById(data.idGuru);
 
-    siswaD === null && errorCb({ status: 404, success: false, msg: 'Siswa tidak ditemukan' }, cb);
-    jenisP === null &&
-      errorCb({ status: 404, success: false, msg: 'Pelanggaran tidak ditemukan' }, cb);
-    guruD === null && errorCb({ status: 404, success: false, msg: 'Guru tidak ditemukan' }, cb);
+      const jenis = {
+        _id: jenisP._id,
+        namaPelanggaran: jenisP.namaPelanggaran,
+        kategori: jenisP.kategori,
+        deskripsi: jenisP.deskripsi,
+        poin: jenisP.poin,
+      };
 
-    const jenis = {
-      _id: jenisP._id,
-      namaPelanggaran: jenisP.namaPelanggaran,
-      kategori: jenisP.kategori,
-      deskripsi: jenisP.deskripsi,
-      poin: jenisP.poin,
-    };
-
-    await Pelanggaran.create({
-      siswa: siswaD.name,
-      jenis: jenis,
-      guru: guruD.name,
-    })
-      .then((pelanggaran) => {
-        successCb({ status: 200, success: true, msg: 'Pelanggaran berhasil ditambahkan' }, cb);
+      await Pelanggaran.create({
+        siswa: siswaD.name,
+        jenis: jenis,
+        guru: guruD.name,
       })
-      .catch((err) => {
-        errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
-      });
+        .then((pelanggaran) => {
+          successCb({ status: 200, success: true, msg: 'Pelanggaran berhasil ditambahkan' }, cb);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } catch (err) {
+      errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
+    }
   },
   readKategoriAll: async (cb) => {
     try {
@@ -165,62 +164,69 @@ module.exports = pelanggaranInstance = {
     }
   },
   readJenisPelanggaranOne: async (id, cb) => {
-    const jenisD = await JenisPelanggaran.findById(id);
-    jenisD === null &&
-      errorCb({ status: 404, success: true, msg: 'Jenis Pelanggaran tidak ditemukan' }, cb);
-    successCb(
-      {
-        status: 200,
-        success: true,
-        data: {
-          ids: jenisD._id,
-          namaPelanggaran: jenisD.namaPelanggaran,
-          kategori: jenisD.kategori,
-          deskripsi: jenisD.deskripsi,
-          poin: jenisD.poin,
+    try {
+      const jenisD = await JenisPelanggaran.findById(id);
+      successCb(
+        {
+          status: 200,
+          success: true,
+          data: {
+            ids: jenisD._id,
+            namaPelanggaran: jenisD.namaPelanggaran,
+            kategori: jenisD.kategori,
+            deskripsi: jenisD.deskripsi,
+            poin: jenisD.poin,
+          },
         },
-      },
-      cb
-    );
+        cb
+      );
+    } catch (err) {
+      errorCb({ status: 404, success: true, msg: 'Jenis Pelanggaran tidak ditemukan' }, cb);
+    }
   },
   readPelanggaranOne: async (id, cb) => {
-    const pelanggaranD = await Pelanggaran.findById(id);
-    pelanggaranD === null &&
-      errorCb({ status: 404, success: false, msg: 'Pelanggaran tidak ditemukan' }, cb);
-    successCb(
-      {
-        status: 200,
-        success: true,
-        data: {
-          ids: pelanggaranD._id,
-          siswa: pelanggaranD.siswa,
-          guru: pelanggaranD.guru,
-          jenis: pelanggaranD.jenis,
-          tgl: pelanggaranD.created_at,
+    try {
+      const pelanggaranD = await Pelanggaran.findById(id);
+      successCb(
+        {
+          status: 200,
+          success: true,
+          data: {
+            ids: pelanggaranD._id,
+            siswa: pelanggaranD.siswa,
+            guru: pelanggaranD.guru,
+            jenis: pelanggaranD.jenis,
+            tgl: pelanggaranD.created_at,
+          },
         },
-      },
-      cb
-    );
+        cb
+      );
+    } catch (err) {
+      errorCb({ status: 404, success: false, msg: 'Pelanggaran tidak ditemukan' }, cb);
+    }
   },
   readPelanggaranSiswa: async (id, cb) => {
-    const siswa = await Siswa.findById(id);
-    siswa === null && errorCb({ status: 404, success: false, msg: 'Siswa tidak ditemukan' }, cb);
-    const pelanggaranSiswa = await Pelanggaran.find({ siswa: siswa.name });
-    let poin = 0;
-    let pel = [];
-    pelanggaranSiswa.map((item) => {
-      pel.push({
-        ids: item._id,
-        siswa: item.siswa,
-        jenis: item.jenis,
-        guru: item.guru,
-        tgl: item.created_at,
+    try {
+      const siswa = await Siswa.findById(id);
+      const pelanggaranSiswa = await Pelanggaran.find({ siswa: siswa.name });
+      let poin = 0;
+      let pel = [];
+      pelanggaranSiswa.map((item) => {
+        pel.push({
+          ids: item._id,
+          siswa: item.siswa,
+          jenis: item.jenis,
+          guru: item.guru,
+          tgl: item.created_at,
+        });
+        poin += item.jenis.poin;
+        if (pel.length === pelanggaranSiswa.length) {
+          return successCb({ status: 200, success: true, data: { pellangaran, poin } }, cb);
+        }
       });
-      poin += item.jenis.poin;
-      if (pel.length === pelanggaranSiswa.length) {
-        return successCb({ status: 200, success: true, data: { pellangaran, poin } }, cb);
-      }
-    });
+    } catch (err) {
+      errorCb({ status: 404, success: false, msg: 'Siswa tidak ditemukan' }, cb);
+    }
   },
   editKategori: async (id, data, cb) => {
     let validasi = [];
@@ -290,84 +296,90 @@ module.exports = pelanggaranInstance = {
       });
   },
   editPelanggaran: async (id, data, cb) => {
-    const jenisP = await JenisPelanggaran.findById(data.idJenisP);
-    jenisP === null &&
-      errorCb({ status: 404, success: false, msg: 'Pelanggaran tidak ditemukan' }, cb);
-    const guruD = await Guru.findById(data.idGuru);
-    guruD === null && errorCb({ status: 404, success: false, msg: 'Guru tidak ditemukan' }, cb);
+    try {
+      const jenisP = await JenisPelanggaran.findById(data.idJenisP);
+      const guruD = await Guru.findById(data.idGuru);
 
-    const jenis = {
-      _id: jenisP._id,
-      namaPelanggaran: jenisP.namaPelanggaran,
-      kategori: jenisP.kategori,
-      deskripsi: jenisP.deskripsi,
-      poin: jenisP.poin,
-    };
+      const jenis = {
+        _id: jenisP._id,
+        namaPelanggaran: jenisP.namaPelanggaran,
+        kategori: jenisP.kategori,
+        deskripsi: jenisP.deskripsi,
+        poin: jenisP.poin,
+      };
 
-    await Pelanggaran.updateOne(
-      { _id: id },
-      {
-        jenis: jenis,
-        guru: guruD.name,
-      }
-    )
-      .then((pelanggaran) => {
-        successCb({ status: 200, success: true, msg: 'Pelanggaran berhasil diperbarui' }, cb);
-      })
-      .catch((err) => {
-        errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
-      });
+      await Pelanggaran.updateOne(
+        { _id: id },
+        {
+          jenis: jenis,
+          guru: guruD.name,
+        }
+      )
+        .then((pelanggaran) => {
+          successCb({ status: 200, success: true, msg: 'Pelanggaran berhasil diperbarui' }, cb);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } catch (error) {
+      errorCb({ status: 500, success: false, msg: 'Internal server error', error }, cb);
+    }
   },
   deleteKategori: async (id, cb) => {
-    const kat = await Kategori.findById(id);
-    kat === null && errorCb({ status: 404, success: false, msg: 'Kategori tidak ditemukan' }, cb);
-    await Kategori.findByIdAndRemove(kat._id)
-      .then(async (del) => {
-        await JenisPelanggaran.find({ kategori: kat.namaKategori })
-          .then(async (jp) => {
-            const jenis = jp.map((j) => ({
-              _id: j._id,
-              namaPelanggaran: j.namaPelanggaran,
-              kategori: j.kategori,
-              deskripsi: j.deskripsi,
-              poin: j.poin,
-            }));
-            await JenisPelanggaran.deleteMany({ kategori: kat.namaKategori });
-            await Pelanggaran.deleteMany({ jenis: jenis });
-            successCb({ status: 200, success: true, msg: 'Kategori telah dihapus' }, cb);
-          })
-          .catch((err) => {
-            errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
-          });
-      })
-      .catch((err) => {
-        errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
-      });
+    try {
+      const kat = await Kategori.findById(id);
+      await Kategori.findByIdAndRemove(kat._id)
+        .then(async (del) => {
+          await JenisPelanggaran.find({ kategori: kat.namaKategori })
+            .then(async (jp) => {
+              const jenis = jp.map((j) => ({
+                _id: j._id,
+                namaPelanggaran: j.namaPelanggaran,
+                kategori: j.kategori,
+                deskripsi: j.deskripsi,
+                poin: j.poin,
+              }));
+              await JenisPelanggaran.deleteMany({ kategori: kat.namaKategori });
+              await Pelanggaran.deleteMany({ jenis: jenis });
+              successCb({ status: 200, success: true, msg: 'Kategori telah dihapus' }, cb);
+            })
+            .catch((err) => {
+              throw err;
+            });
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } catch (error) {
+      errorCb({ status: 500, success: false, msg: 'Internal server error', err }, cb);
+    }
   },
   deleteJenisPelanggaran: async (id, cb) => {
-    const jenis = await JenisPelanggaran.findById(id);
-    jenis === null &&
+    try {
+      const jenis = await JenisPelanggaran.findById(id);
+      await JenisPelanggaran.findByIdAndRemove(id)
+        .then(async (del) => {
+          const jenisD = {
+            _id: jenis._id,
+            namaPelanggaran: jenis.namaPelanggaran,
+            kategori: jenis.kategori,
+            deskripsi: jenis.deskripsi,
+            poin: jenis.poin,
+          };
+          await Pelanggaran.deleteMany({ jenis: jenisD })
+            .then((del) => {
+              successCb({ status: 200, success: true, msg: 'Jenis pelanggaran telah dihapus' }, cb);
+            })
+            .catch((err) => {
+              throw err;
+            });
+        })
+        .catch((err) => {
+          errorCb({ status: 500, success: false, msg: 'Internal server error' }, cb);
+        });
+    } catch (error) {
       errorCb({ status: 404, success: true, msg: 'Jenis Pelanggaran tidak ditemukan' }, cb);
-    await JenisPelanggaran.findByIdAndRemove(id)
-      .then(async (del) => {
-        const jenisD = {
-          _id: jenis._id,
-          namaPelanggaran: jenis.namaPelanggaran,
-          kategori: jenis.kategori,
-          deskripsi: jenis.deskripsi,
-          poin: jenis.poin,
-        };
-        await Pelanggaran.deleteMany({ jenis: jenisD })
-          .then((del) => {
-            successCb({ status: 200, success: true, msg: 'Jenis pelanggaran telah dihapus' }, cb);
-          })
-          .catch((err) => {
-            errorCb({ status: 500, success: false, msg: 'Internal server error' }, cb);
-          });
-      })
-      .catch((err) => {
-        errorCb({ status: 500, success: false, msg: 'Internal server error' }, cb);
-      });
+    }
   },
   deletePelanggaran: async (id, cb) => {
     await Pelanggaran.findByIdAndRemove(id)
