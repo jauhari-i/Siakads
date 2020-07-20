6bf7a9a77d788d51608f007ffed671664301b25c
Irfan Jauhari <kangrykong123@gmail.com>
2020-07-20 16:27:04 +0700

    lapor init

--- src/bin/app.js
+++ src/bin/app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bp = require('body-parser');
-const cors = require('cors');
-const log = require('morgan');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bp = require("body-parser");
+const cors = require("cors");
+const log = require("morgan");
+const path = require("path");
 const app = express();
-const port = require('../constants/port');
-require('dotenv').config();
+const port = require("../constants/port");
+require("dotenv").config();
 
 app.use(cors());
-app.use(log('dev'));
+app.use(log("dev"));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: false }));
 
@@ -19,28 +19,29 @@ mongoose.connect(
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   },
   (err) => {
     if (err) {
       console.log(err);
     } else {
-      console.log('Connected to database');
+      console.log("Connected to database");
     }
   }
 );
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public/', 'underconstruction.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/", "underconstruction.html"))
 );
 
-app.use('/api', require('../apis/index'));
+app.use("/api", require("../apis/index"));
 
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   res.status(404).json({
-    message: req.method + ' ' + req.url + ' not found',
-    error: 'NoEndpointExist',
+    message: req.method + " " + req.url + " not found",
+    error: "NoEndpointExist",
     code: 404,
   });
   next();
---
+++ src/services/siswa/index.js
@@ -0,0 +1,3 @@
+module.exports = siswa = {
+  laporSarana: require("./laporSarana"),
+};
